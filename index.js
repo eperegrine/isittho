3ae4a7471a523e4f89782eb7f91017b4f86a1c2e
emilyperegrine <peregrineemily@gmail.com>
2017-08-10 12:20:08 +0100

    Added heroku stuff 2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
