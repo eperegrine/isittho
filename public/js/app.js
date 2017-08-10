44354401869724eac90d4bfccfc02916f4397b3f
emilyperegrine <peregrineemily@gmail.com>
2017-08-10 11:40:41 +0100

    Basic layout complete

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,10 @@ $(document).ready(function () {
     var app = new Vue({
         el: '#app',
         data: {
-            message: 'Hello Vue!'
+            bgc: {
+                background: dodieYellow
+            },
+            score: 0
         }
     });
 });
